bb95ee9d405a5b6f6742c8ca335a580473a02d99
HeyBail <Bill331333@gmail.com>
2024-11-18 14:18:35 -0600

    Lock picking minigame update

--- PogoKnight/Assets/Scripts/LockpickingPuzzle/LockPickingStick.cs
+++ PogoKnight/Assets/Scripts/LockpickingPuzzle/LockPickingStick.cs
@@ -6,12 +6,9 @@ public class LockPickingStick : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("your mother");
         if (collision.gameObject.tag == "Goal")
         {
-            Debug.Log("You Win");
-            // win or next level
-            //collision.gameObject.GetComponent<Goal>().complete;
+            MinigameManager_Lockpicking.LevelComplete();
         }
     }
 }
--- PogoKnight/Assets/Scripts/LockpickingPuzzle/MinigameManager_Lockpicking.cs
+++ PogoKnight/Assets/Scripts/LockpickingPuzzle/MinigameManager_Lockpicking.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MinigameManager_Lockpicking : MonoBehaviour
 {
     [SerializeField]
+    static LockPickingState lockPickingState = LockPickingState.level1;
+    
+    [SerializeField]
     Camera miniGameCamera;
-    // Start is called before the first frame update
+
     void Start()
     {
         miniGameCamera.gameObject.SetActive(true);
@@ -16,5 +20,29 @@ public class MinigameManager_Lockpicking : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static void LevelComplete() 
+    { 
+        switch (MinigameManager_Lockpicking.lockPickingState) //could make the blocks increase in speed, takes 3 hits to move to next one. could also switch direction
+        {
+            case LockPickingState.level1:
+                Debug.Log("level 1 complete");
+                lockPickingState = LockPickingState.level2;
+                break;
+            case LockPickingState.level2:
+                Debug.Log("level 2 complete");
+                lockPickingState = LockPickingState.level3;
+                break;
+            case LockPickingState.level3:
+                winLevel();
+                break;
+        }
+    }
+
+    private static void winLevel() 
+    {
+        Debug.Log("you win");
+        SceneManager.LoadScene("Level1");
     }
 }
